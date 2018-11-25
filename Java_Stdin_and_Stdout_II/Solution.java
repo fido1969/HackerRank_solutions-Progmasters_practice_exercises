1dd1c96eedd9a0e8642ad47f073f94619702b97a
peti8 <45337231+peti8@users.noreply.github.com>
2018-11-25 18:42:43 +0100

    Update Solution.java

--- Java_Stdin_and_Stdout_II/Solution.java
+++ Java_Stdin_and_Stdout_II/Solution.java
@@ -5,6 +5,9 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
